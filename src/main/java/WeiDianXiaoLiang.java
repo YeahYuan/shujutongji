3d11ed4eabeab4512cac657dc6f8d6fad809b7f9
yuannide <13760208237@sina.cn>
2022-11-18 16:16:43 +0800

    杂志销量

--- src/main/java/WeiDianXiaoLiang.java
+++ src/main/java/WeiDianXiaoLiang.java
@@ -5,12 +5,9 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class WeiDianXiaoLiang {
-
     private static final DateTimeFormatter df = DateTimeFormatter.ofPattern("MM-dd HH:mm:ss");
-
     public static void main(String[] args) throws InterruptedException {
         String urlParam = "https://thor.weidian.com/detail/getItemSkuInfo/1.0?param=%7B%22itemId%22%3A%225824125384%22%7D&wdtoken=24fd0bc9&_=1668703085412";
-
         while (true) {
             String resStr = HttpTools.get(urlParam);
             JSONObject strJson = JSONObject.parseObject(resStr);
@@ -21,13 +18,10 @@ public class WeiDianXiaoLiang {
                 printOne(jsonObject);
             }
             System.out.println();
-            Thread.sleep(1000);
+            Thread.sleep(1000 * 60);
         }
     }
-
     private static void printOne(JSONObject jsonObject) {
         System.out.print(jsonObject.getJSONObject("skuInfo").getString("stock") + "\t");
     }
-
-
 }
